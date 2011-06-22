96336f1ee0a1178b62c0990eed3b7fe4bca8b7c4
Moises Perez <mxpxgx@gmail.com>
2011-06-21 19:04:22 -0700

    Small fix on RecordActivity (test)

--- src/edu/incense/android/ui/RecordActivity.java
+++ src/edu/incense/android/ui/RecordActivity.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
